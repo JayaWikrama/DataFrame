9e51da2a88d075e6751d40ac13a9135d6b38654d
JayaWikrama <jayawikrama89@gmail.com>
2024-07-11 21:38:24 +0700

    FirstCommit::proggress --> Prototype fungsi.

---
+++ include/data-frame.hpp
@@ -0,0 +1,84 @@
+#ifndef __DATA_FRAME_HPP__
+#define __DATA_FRAME_HPP__
+
+#include <vector>
+
+class DataFrame {
+  private:
+    unsigned char type;
+    size_t sz;
+    std::vector <unsigned char> data;
+    void *exeFunc;
+    void *exeFuncParam;
+    void *postFunc;
+    void *postFuncParam;
+    DataFrame *next;
+  public:
+    typedef enum _FRAME_TYPE_t {
+      FRAME_TYPE_START_BYTES = 0,
+      FRAME_TYPE_CONTENT_LENGTH,
+      FRAME_TYPE_COMMAND,
+      FRAME_TYPE_DATA,
+      FRAME_TYPE_STOP_BYTES,
+      FRAME_TYPE_VALIDATOR
+    } FRAME_TYPE_t;
+
+    DataFrame();
+
+    DataFrame(DataFrame::FRAME_TYPE_t type);
+
+    DataFrame(DataFrame::FRAME_TYPE_t type,
+              size_t sz);
+
+    DataFrame(DataFrame::FRAME_TYPE_t type,
+              size_t sz,
+              const unsigned char *data);
+
+    DataFrame(DataFrame::FRAME_TYPE_t type,
+              size_t sz,
+              const unsigned char *data,
+              const void *exeFunc,
+              const void *exeFuncParam);
+
+    DataFrame(DataFrame::FRAME_TYPE_t type,
+              size_t sz,
+              const unsigned char *data,
+              const void *exeFunc,
+              const void *exeFuncParam,
+              const void *postFunc,
+              const void *postFuncParam
+              );
+
+    ~DataFrame();
+
+    void setType(DataFrame::FRAME_TYPE_t type);
+
+    void setSize(size_t sz);
+
+    void setRefference(const unsigned char *reference, size_t sz);
+
+    void setRefference(const std::vector<unsigned char> reference);
+
+    void setData(const unsigned char *data, size_t sz);
+
+    void setData(const std::vector<unsigned char> data, size_t sz);
+
+    void setExecuteFunction(const void *_func, void *_param);
+
+    void setPostExecuteFunction(const void *_func, void *_param);
+
+    DataFrame::FRAME_TYPE_t getType();
+
+    size_t getSize();
+
+    size_t getReference(unsigned char *reference, size_t sizeOfReference);
+
+    size_t getReference(std::vector<unsigned char> reference);
+
+    size_t getData(unsigned char *data, size_t sizeOfReference);
+
+    size_t getData(std::vector<unsigned char> data);
+
+};
+
+#endif
