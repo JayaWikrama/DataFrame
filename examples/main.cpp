e70b9b41971c9ce0e5bde89ea837d38e620a525a
Jaya Wikrama <jayawikrama89@gmail.com>
2024-07-12 23:54:39 +0700

    develop::proggress --> Penyesuaian example untuk menguji penambahan Operator Overloading (+).

--- examples/main.cpp
+++ examples/main.cpp
@@ -5,9 +5,7 @@
 
 int main(int argc, char **argv){
     DataFrame myFrame(DataFrame::FRAME_TYPE_START_BYTES);
-    myFrame += DataFrame(DataFrame::FRAME_TYPE_COMMAND);
-    myFrame += DataFrame(DataFrame::FRAME_TYPE_DATA);
-    myFrame += DataFrame(DataFrame::FRAME_TYPE_VALIDATOR);
+    myFrame += DataFrame(DataFrame::FRAME_TYPE_COMMAND) + DataFrame(DataFrame::FRAME_TYPE_DATA) + DataFrame(DataFrame::FRAME_TYPE_VALIDATOR);
     std::cout << myFrame.getDataFrameFormat() << std::endl;
     return 0;
 }
