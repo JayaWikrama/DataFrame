c8b4af78568e075be09546228e94281403439dad
JayaWikrama <jayawikrama89@gmail.com>
2024-07-18 21:46:44 +0700

    modify simple example.

--- examples/main.cpp
+++ examples/main.cpp
@@ -3,9 +3,20 @@
 
 #include "data-frame.hpp"
 
+void testExe(DataFrame &obj, void *param){
+    std::cout << "[Exe] Type: " << obj.getType() << std::endl;
+}
+
+void testPost(DataFrame &obj, void *param){
+    std::cout << "[Post] Type: " << obj.getType() << std::endl;
+}
+
 int main(int argc, char **argv){
     DataFrame myFrame(DataFrame::FRAME_TYPE_START_BYTES);
     myFrame += DataFrame(DataFrame::FRAME_TYPE_COMMAND) + DataFrame(DataFrame::FRAME_TYPE_DATA) + DataFrame(DataFrame::FRAME_TYPE_VALIDATOR);
+    myFrame.setExecuteFunction((const void *) &testExe, nullptr);
+    myFrame.setPostExecuteFunction((const void *) &testPost, nullptr);
     std::cout << myFrame.getDataFrameFormat() << std::endl;
+    myFrame.execute();
     return 0;
 }
